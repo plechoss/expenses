baea8994f741cefe75fdf391ebe08a6fdd37292c
plechoss <plaskacz.panda@gmail.com>
2022-10-05 09:49:51 +0200

    Added web history creation

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/expenses/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
